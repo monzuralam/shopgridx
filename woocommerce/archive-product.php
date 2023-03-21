54d06d08ebe47045b3143b9a4c1ed3b3aa01a3ab
monzuralam <monzur707@gmail.com>
2023-03-21 12:22:59 +0600

    archive product has been dynamic

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -19,97 +19,111 @@
 defined('ABSPATH') || exit;
 get_header('shop');
 ?>
-<div class="container my-5">
-	<div class="row">
-		<div class="col-md-8 order-1 order-md-2">
-			<?php
-			/**
-			 * Hook: woocommerce_before_main_content.
-			 *
-			 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-			 * @hooked woocommerce_breadcrumb - 20
-			 * @hooked WC_Structured_Data::generate_website_data() - 30
-			 */
-			do_action('woocommerce_before_main_content');
-			?>
-			<header class="woocommerce-products-header">
-				<?php if (apply_filters('woocommerce_show_page_title', true)) : ?>
-					<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-				<?php endif; ?>
-
+<section class="product-grids section">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 col-12">
 				<?php
 				/**
-				 * Hook: woocommerce_archive_description.
+				 * Hook: woocommerce_sidebar.
 				 *
-				 * @hooked woocommerce_taxonomy_archive_description - 10
-				 * @hooked woocommerce_product_archive_description - 10
+				 * @hooked woocommerce_get_sidebar - 10
 				 */
-				do_action('woocommerce_archive_description');
+				do_action('woocommerce_sidebar');
 				?>
-			</header>
-			<?php
-				if (woocommerce_product_loop()) {
-
-					/**
-					 * Hook: woocommerce_before_shop_loop.
-					 *
-					 * @hooked woocommerce_output_all_notices - 10
-					 * @hooked woocommerce_result_count - 20
-					 * @hooked woocommerce_catalog_ordering - 30
-					 */
-					do_action('woocommerce_before_shop_loop');
-
-					woocommerce_product_loop_start();
-
-					if (wc_get_loop_prop('total')) {
-						while (have_posts()) {
-							the_post();
-
-							/**
-							 * Hook: woocommerce_shop_loop.
-							 */
-							do_action('woocommerce_shop_loop');
-
-							wc_get_template_part('content', 'product');
-						}
-					}
-
-					woocommerce_product_loop_end();
-
-					/**
-					 * Hook: woocommerce_after_shop_loop.
-					 *
-					 * @hooked woocommerce_pagination - 10
-					 */
-					do_action('woocommerce_after_shop_loop');
-				} else {
-					/**
-					 * Hook: woocommerce_no_products_found.
-					 *
-					 * @hooked wc_no_products_found - 10
-					 */
-					do_action('woocommerce_no_products_found');
-				}
-
+			</div>
+			<div class="col-lg-9 col-12">
+				<?php
 				/**
-				 * Hook: woocommerce_after_main_content.
+				 * Hook: woocommerce_before_main_content.
 				 *
-				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 * @hooked WC_Structured_Data::generate_website_data() - 30
 				 */
-				do_action('woocommerce_after_main_content');
-			?>
-		</div>
-		<div class="col-md-4 order-2 order-md-1">
-			<?php
-			/**
-			 * Hook: woocommerce_sidebar.
-			 *
-			 * @hooked woocommerce_get_sidebar - 10
-			 */
-			do_action('woocommerce_sidebar');
-			?>
+				do_action('woocommerce_before_main_content');
+				?>
+				<div class="product-grids-head">
+					<div class="product-grid-topbar">
+						<div class="row align-items-center">
+							<div class="col-lg-7 col-md-8 col-12">
+								<div class="product-sorting">
+									<label for="sorting">Sort by:</label>
+									<select class="form-control" id="sorting">
+										<option>Popularity</option>
+										<option>Low - High Price</option>
+										<option>High - Low Price</option>
+										<option>Average Rating</option>
+										<option>A - Z Order</option>
+										<option>Z - A Order</option>
+									</select>
+									<h3 class="total-show-product">Showing: <span>1 - 12 items</span></h3>
+								</div>
+							</div>
+							<div class="col-lg-5 col-md-4 col-12">
+							</div>
+						</div>
+					</div>
+					<div class="product-area">
+						<div class="row">
+							<?php
+								if (woocommerce_product_loop()) {
+
+									/**
+									 * Hook: woocommerce_before_shop_loop.
+									 *
+									 * @hooked woocommerce_output_all_notices - 10
+									 * @hooked woocommerce_result_count - 20
+									 * @hooked woocommerce_catalog_ordering - 30
+									 */
+									do_action('woocommerce_before_shop_loop');
+					
+									woocommerce_product_loop_start();
+					
+									if (wc_get_loop_prop('total')) {
+										while (have_posts()) {
+											the_post();
+					
+											/**
+											 * Hook: woocommerce_shop_loop.
+											 */
+											do_action('woocommerce_shop_loop');
+					
+											wc_get_template_part('content', 'product');
+										}
+									}
+					
+									woocommerce_product_loop_end();
+					
+									/**
+									 * Hook: woocommerce_after_shop_loop.
+									 *
+									 * @hooked woocommerce_pagination - 10
+									 */
+									do_action('woocommerce_after_shop_loop');
+								} else {
+									/**
+									 * Hook: woocommerce_no_products_found.
+									 *
+									 * @hooked wc_no_products_found - 10
+									 */
+									do_action('woocommerce_no_products_found');
+								}
+							?>
+						</div>
+					</div>
+				</div>
+				<?php
+					/**
+					 * Hook: woocommerce_after_main_content.
+					 *
+					 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+					 */
+					do_action('woocommerce_after_main_content');
+				?>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
 <?php
 get_footer('shop');
---
+++ woocommerce/content-product.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The template for displaying product content within loops
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.6.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+// Ensure visibility.
+if ( empty( $product ) || ! $product->is_visible() ) {
+	return;
+}
+?>
+<div <?php wc_product_class( 'col-lg-4 col-md-6 col-12', $product ); ?> >
+	<!-- Start Single Product -->
+	<div class="single-product">
+		<div class="product-image">
+			<?php the_post_thumbnail(); ?>
+			<div class="button">
+				<a href="<?php echo $product->add_to_cart_url() ?>" class="btn"><i class="lni lni-cart"></i> Add to Cart</a>
+			</div>
+		</div>
+		<div class="product-info">
+			<?php
+				$terms = get_the_terms( $post->ID, 'product_cat' );
+				if ( $terms && ! is_wp_error( $terms ) ) : 
+					$cat_links = array();
+					foreach ( $terms as $term ) {
+							$cat_links[] = '<a class="label bg-terciary" href="/?product_cat='.$term->slug.'" title="'.$term->name.'">'.$term->name.'</a>';
+					}
+					$on_cat = join( " ", $cat_links );
+			?>
+			<span class="category"><?php echo $on_cat; ?></span>
+			<?php endif; ?>
+
+			<h4 class="title">
+				<a href="product-grids.html"><?php the_title(); ?></a>
+			</h4>
+			<ul class="review">
+				<li><i class="lni lni-star-filled"></i></li>
+				<li><i class="lni lni-star-filled"></i></li>
+				<li><i class="lni lni-star-filled"></i></li>
+				<li><i class="lni lni-star-filled"></i></li>
+				<li><i class="lni lni-star"></i></li>
+				<li><span>4.0 Review(s)</span></li>
+			</ul>
+			<div class="price">
+				<?php
+					$product = wc_get_product( get_the_ID() );
+					echo $product->get_price_html();
+				?>
+			</div>
+		</div>
+	</div>
+	<!-- End Single Product -->
+</div>
