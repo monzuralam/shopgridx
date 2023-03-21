ea7281d39eb81b8db142c8ac8a7a1e074c54e21f
monzuralam <monzur707@gmail.com>
2023-03-21 10:29:18 +0600

    sidebar dynamic

--- sidebar.php
+++ sidebar.php
@@ -1,84 +1,15 @@
 <aside class="">
-    <div class="sidebar blog-grid-page">
-        <!-- Start Single Widget -->
-        <div class="widget search-widget">
-            <h5 class="widget-title">Search This Site</h5>
-            <form action="#">
-                <input type="text" placeholder="Search Here...">
-                <button type="submit"><i class="lni lni-search-alt"></i></button>
-            </form>
-        </div>
-        <!-- End Single Widget -->
-        <!-- Start Single Widget -->
-        <div class="widget popular-feeds">
-            <h5 class="widget-title">Featured Posts</h5>
-            <div class="popular-feed-loop">
-                <div class="single-popular-feed">
-                    <div class="feed-desc">
-                        <a class="feed-img" href="blog-single-sidebar.html">
-                            <img src="https://via.placeholder.com/200x200" alt="#">
-                        </a>
-                        <h6 class="post-title"><a href="blog-single-sidebar.html">What information is
-                                needed for shipping?</a></h6>
-                        <span class="time"><i class="lni lni-calendar"></i> 05th Nov 2023</span>
-                    </div>
-                </div>
-                <div class="single-popular-feed">
-                    <div class="feed-desc">
-                        <a class="feed-img" href="blog-single-sidebar.html">
-                            <img src="https://via.placeholder.com/200x200" alt="#">
-                        </a>
-                        <h6 class="post-title"><a href="blog-single-sidebar.html">Interesting fact about
-                                gaming consoles</a></h6>
-                        <span class="time"><i class="lni lni-calendar"></i> 24th March 2023</span>
-                    </div>
-                </div>
-                <div class="single-popular-feed">
-                    <div class="feed-desc">
-                        <a class="feed-img" href="blog-single-sidebar.html">
-                            <img src="https://via.placeholder.com/200x200" alt="#">
-                        </a>
-                        <h6 class="post-title"><a href="blog-single-sidebar.html">Electronics,
-                                instrumentation & control engineering </a></h6>
-                        <span class="time"><i class="lni lni-calendar"></i> 30th Jan 2023</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Single Widget -->
-        <!-- Start Single Widget -->
-        <div class="widget categories-widget">
-            <h5 class="widget-title">Top Categories</h5>
-            <ul class="custom">
-                <li>
-                    <a href="javascript:void(0)">Editor's Choice</a><span>(24)</span>
-                </li>
-                <li>
-                    <a href="javascript:void(0)">Electronics</a><span>(12)</span>
-                </li>
-                <li>
-                    <a href="javascript:void(0)">Industrial Design</a><span>(5)</span>
-                </li>
-                <li>
-                    <a href="javascript:void(0)">Secure Payments Online</a><span>(15)</span>
-                </li>
-                <li>
-                    <a href="javascript:void(0)">Online Shopping</a><span>(7)</span>
-                </li>
-            </ul>
-        </div>
-        <!-- End Single Widget -->
-        <!-- Start Single Widget -->
-        <div class="widget popular-tag-widget">
-            <h5 class="widget-title">Popular Tags</h5>
-            <div class="tags">
-                <a href="javascript:void(0)">#electronics</a>
-                <a href="javascript:void(0)">#cpu</a>
-                <a href="javascript:void(0)">#gadgets</a>
-                <a href="javascript:void(0)">#wearables</a>
-                <a href="javascript:void(0)">#smartphones</a>
-            </div>
-        </div>
-        <!-- End Single Widget -->
+    <div class="sidebar <?php (is_woocommerce()) ? esc_attr_e('product-sidebar') : esc_attr_e('blog-grid-page');  ?>">
+        <?php
+        if (is_woocommerce()) :
+            if( is_active_sidebar( 'shop-sidebar' ) ):
+                dynamic_sidebar( 'shop-sidebar' );
+            endif;
+        else :
+            if( is_active_sidebar( 'sidebar' ) ):
+                dynamic_sidebar( 'sidebar' );
+            endif;
+        endif;
+        ?>
     </div>
 </aside>
