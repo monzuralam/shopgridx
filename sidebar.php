df8eb5c2fec3ba3325c869bb22b05a4ba9a5d84f
monzuralam <monzur707@gmail.com>
2023-11-07 18:26:18 +0600

    check function & handle fatal error

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <aside class="">
-    <div class="sidebar <?php (is_woocommerce()) ? esc_attr_e('product-sidebar') : esc_attr_e('blog-grid-page');  ?>">
+    <div class="sidebar <?php (function_exists('is_woocommerce')) ? esc_attr_e('product-sidebar') : esc_attr_e('blog-grid-page');  ?>">
         <?php
-        if (is_woocommerce()) :
+        if (function_exists('is_woocommerce')) :
             if( is_active_sidebar( 'shop-sidebar' ) ):
                 dynamic_sidebar( 'shop-sidebar' );
             endif;
